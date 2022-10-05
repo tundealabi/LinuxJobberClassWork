c302b14464c042292003e7968b46468270dab33a
Tunde Alabi <tundealabi4780@gmail.com>
2022-10-05 11:52:38 +0100

    refactor slider logic

--- src/components/interests/Interests.jsx
+++ src/components/interests/Interests.jsx
@@ -1,54 +1,92 @@
-import './interest.css'
-import checked from '../../assets/checked.svg'
+import './interest.css';
+import checked from '../../assets/checked.svg';
 import Sliderr from '../sliderr/Slider';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 // import rangeData from '../../static/rangeData';
 
 const Interests = () => {
-  const [ rangeValue, setRangeValue ] = useState(0)
-  const [ rangeValue1, setRangeValue1 ] = useState(0)
-  const [ rangeValue2, setRangeValue2 ] = useState(0)
-  const [ rangeValue3, setRangeValue3 ] = useState(0)
-  const [ rangeValue4, setRangeValue4 ] = useState(0)
-  const [ rangeValue5, setRangeValue5 ] = useState(0)
-  const [ rangeValue6, setRangeValue6 ] = useState(0)
-  const [ rangeValue7, setRangeValue7 ] = useState(0)
-  const [ equal, setEqual ] = useState(true)
+  // const [rangeValue, setRangeValue] = useState(0);
+  // const [rangeValue1, setRangeValue1] = useState(0);
+  // const [rangeValue2, setRangeValue2] = useState(0);
+  // const [rangeValue3, setRangeValue3] = useState(0);
+  // const [rangeValue4, setRangeValue4] = useState(0);
+  // const [rangeValue5, setRangeValue5] = useState(0);
+  // const [rangeValue6, setRangeValue6] = useState(0);
+  // const [rangeValue7, setRangeValue7] = useState(0);
+  const [ranges, setRanges] = useState([
+    { title: 'Cyber Security', value: 0 },
+    { title: 'Project Manager', value: 0 },
+    { title: 'Developer', value: 0 },
+    { title: 'Product Manager', value: 0 },
+    { title: 'DevOps', value: 0 },
+    { title: 'Marketer', value: 0 },
+    { title: 'Designer', value: 0 },
+    { title: 'Writer', value: 0 },
+  ]);
+  // const [equal, setEqual] = useState(true);
+  const [isError, setIserror] = useState(true);
 
-  const onSliderChange = (e , newValue) => {
-     setRangeValue(parseInt(e.target.value))
-     newValue = parseInt(e.target.value)
-       if (newValue === rangeValue1|| rangeValue2 || rangeValue3 || rangeValue4 || rangeValue5|| rangeValue6 || rangeValue7){
-        setEqual(false)
-        console.log(equal)
-       } else {
-        setEqual(true)
-       }
-   }
-   const onSliderChange1 = (e, newValue) => {
-     setRangeValue1(parseInt(e.target.value))
-   }
-   const onSliderChange2 = (e, newValue) => {
-     setRangeValue2(parseInt(e.target.value))
-   }
-   const onSliderChange3 = (e, newValue) => {
-     setRangeValue3(parseInt(e.target.value))
-   }
-   const onSliderChange4 = (e, newValue) => {
-     setRangeValue4(parseInt(e.target.value))
-   }
-   const onSliderChange5 = (e, newValue) => {
-     setRangeValue5(parseInt(e.target.value))
-   }
+  const handlesliderChange = (ev) => {
+    const { value, name } = ev.target;
+    setRanges((prevState) => {
+      return prevState.map((range) => {
+        if (range.title == name) {
+          range.value = parseInt(value);
+        }
+        return range;
+      });
+    });
+  };
 
-   const onSliderChange6 = (e, newValue) => {
-     setRangeValue6(parseInt(e.target.value))
-   }
-   const onSliderChange7 = (e, newValue) => {
-     setRangeValue7(parseInt(e.target.value))
-   }
-  
+  useEffect(() => {
+    const uniques = new Set();
+    ranges.forEach((range) => {
+      uniques.add(range.value);
+    });
+    setIserror(uniques.size != ranges.length);
+  }, [ranges]);
+
+  // const onSliderChange = (e, newValue) => {
+  //   setRangeValue(parseInt(e.target.value));
+  //   newValue = parseInt(e.target.value);
+  //   if (
+  //     newValue === rangeValue1 ||
+  //     rangeValue2 ||
+  //     rangeValue3 ||
+  //     rangeValue4 ||
+  //     rangeValue5 ||
+  //     rangeValue6 ||
+  //     rangeValue7
+  //   ) {
+  //     setEqual(false);
+  //     console.log(equal);
+  //   } else {
+  //     setEqual(true);
+  //   }
+  // };
+  // const onSliderChange1 = (e, newValue) => {
+  //   setRangeValue1(parseInt(e.target.value));
+  // };
+  // const onSliderChange2 = (e, newValue) => {
+  //   setRangeValue2(parseInt(e.target.value));
+  // };
+  // const onSliderChange3 = (e, newValue) => {
+  //   setRangeValue3(parseInt(e.target.value));
+  // };
+  // const onSliderChange4 = (e, newValue) => {
+  //   setRangeValue4(parseInt(e.target.value));
+  // };
+  // const onSliderChange5 = (e, newValue) => {
+  //   setRangeValue5(parseInt(e.target.value));
+  // };
+
+  // const onSliderChange6 = (e, newValue) => {
+  //   setRangeValue6(parseInt(e.target.value));
+  // };
+  // const onSliderChange7 = (e, newValue) => {
+  //   setRangeValue7(parseInt(e.target.value));
+  // };
 
   return (
     <div className="interest">
@@ -56,72 +94,85 @@ const Interests = () => {
         <img src={checked} alt="a" className="ccc " />
       </div>
       <div className="sub-interst">
-        <p className="int-txt">Score your level of interest in these job titles:</p>
+        <p className="int-txt">
+          Score your level of interest in these job titles:
+        </p>
         <div className="slider-range-container">
-        <Sliderr
-            title = {'Cyber Security'}
-            min = {1}
-            max = {10}
-            step = {1}
-            value = {rangeValue}
-            onChangeValue = {onSliderChange}
+          {ranges.map((range) => (
+            <Sliderr
+              key={range.title}
+              title={range.title}
+              min={1}
+              max={10}
+              step={1}
+              value={range.value}
+              onChangeValue={handlesliderChange}
+            />
+          ))}
+          {/* <Sliderr
+            title={'Cyber Security'}
+            min={1}
+            max={10}
+            step={1}
+            value={rangeValue}
+            onChangeValue={onSliderChange}
           />
           <Sliderr
-            title = {'Project Manager'}
-            min = {1}
-            max = {10}
-            step = {1}
-            value = {rangeValue1}
-            onChangeValue = {onSliderChange1}
+            title={'Project Manager'}
+            min={1}
+            max={10}
+            step={1}
+            value={rangeValue1}
+            onChangeValue={onSliderChange1}
           />
           <Sliderr
-            title = {'Developer'}
-            min = {1}
-            max = {10}
-            step = {1}
-            value = {rangeValue2}
-            onChangeValue = {onSliderChange2}
+            title={'Developer'}
+            min={1}
+            max={10}
+            step={1}
+            value={rangeValue2}
+            onChangeValue={onSliderChange2}
           />
           <Sliderr
-            title = {'Product Manager'}
-            min = {1}
-            max = {10}
-            step = {1}
-            value = {rangeValue3}
-            onChangeValue = {onSliderChange3}
+            title={'Product Manager'}
+            min={1}
+            max={10}
+            step={1}
+            value={rangeValue3}
+            onChangeValue={onSliderChange3}
           />
           <Sliderr
-            title = {'DevOps'}
-            min = {1}
-            max = {10}
-            step = {1}
-            value = {rangeValue4}
-            onChangeValue = {onSliderChange4}
+            title={'DevOps'}
+            min={1}
+            max={10}
+            step={1}
+            value={rangeValue4}
+            onChangeValue={onSliderChange4}
           />
           <Sliderr
-            title = {'Marketer'}
-            min = {1}
-            max = {10}
-            step = {1}
-            value = {rangeValue5}
-            onChangeValue = {onSliderChange5}
+            title={'Marketer'}
+            min={1}
+            max={10}
+            step={1}
+            value={rangeValue5}
+            onChangeValue={onSliderChange5}
           />
           <Sliderr
-            title = {'Designer'}
-            min = {1}
-            max = {10}
-            step = {1}
-            value = {rangeValue6}
-            onChangeValue = {onSliderChange6}
+            title={'Designer'}
+            min={1}
+            max={10}
+            step={1}
+            value={rangeValue6}
+            onChangeValue={onSliderChange6}
           />
           <Sliderr
-            title = {'Writer'}
-            min = {1}
-            max = {10}
-            step = {1}
-            value = {rangeValue7}
-            onChangeValue = {onSliderChange7}
-          />
+            title={'Writer'}
+            min={1}
+            max={10}
+            step={1}
+            value={rangeValue7}
+            onChangeValue={onSliderChange7}
+          /> */}
         </div>
       </div>
 
@@ -129,18 +180,22 @@ const Interests = () => {
         style={{
           fontSize: 18,
           fontStyle: 'italic',
-          color: !equal ? 'var(--red)' :'var(--sliderText)',
-          marginTop: 10
+          color: isError ? 'var(--red)' : 'var(--sliderText)',
+          marginTop: 10,
         }}
-      >* You cannot pick a number twice.</p>
-      <Link to='/my-profile'>
+      >
+        * You cannot pick a number twice.
+      </p>
+      <Link to="/my-profile">
         <button className="next">
           <p>Done</p>
-          <span className="material-symbols-outlined arrow">arrow_forward_ios</span>
+          <span className="material-symbols-outlined arrow">
+            arrow_forward_ios
+          </span>
         </button>
       </Link>
     </div>
-  )
-}
+  );
+};
 
 export default Interests;
--- src/components/sliderr/Slider.jsx
+++ src/components/sliderr/Slider.jsx
@@ -1,40 +1,40 @@
-import './slider.css'
+import './slider.css';
 // import Slider from '@mui/material/Slider';
 
 const Sliderr = (props) => {
-   const { min, max ,step, value, title, onChangeValue } = props
+  const { min, max, step, value, title, onChangeValue } = props;
 
-   const handleChange = () => e => {
-      onChangeValue(e)
-   }
+  const handleChange = () => (e) => {
+    onChangeValue(e);
+  };
 
   return (
-    <div className='slider-container'>
-        <div className='slider-desc'>
-          <p className='slider-title'>{title} </p>
-          <span
+    <div className="slider-container">
+      <div className="slider-desc">
+        <p className="slider-title">{title} </p>
+        <span
           style={{
-            color : '#002851'
+            color: '#002851',
           }}
-          >
-            {value}/{max}
-          </span>
-        </div>
-        <div>
-          <input  
-            className= 'sliderInput'
-            type="range" 
-            name="paul" 
-            max={max}
-            min={min}
-            step={step}
-            value={value}
-            id="paul"
-            onChange={handleChange(max)}
-          />
-        </div>
+        >
+          {value}/{max}
+        </span>
+      </div>
+      <div>
+        <input
+          className="sliderInput"
+          type="range"
+          name={title}
+          max={max}
+          min={min}
+          step={step}
+          value={value}
+          id="paul"
+          onChange={handleChange(max)}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Sliderr;
